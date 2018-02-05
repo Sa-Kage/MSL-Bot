e3807dc43ab355a6ff45621e0b475a2bf3e7ef2a
Sa'Kagé <aml-os@gmx.de>
2018-02-05 23:24:34 +0100

    Added main GUI
    Task icon not showing

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Created by SharpDevelop.
+ * Author: Alexander Lübbe
+ * Date: 05.02.2018
+ * Time: 21:54
+ */
+using System;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace MSL_Bot
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class Program
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+	}
+}
